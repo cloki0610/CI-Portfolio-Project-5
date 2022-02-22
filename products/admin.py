4abbddc945a3b270913b428fa5370783fb53c32b
Rocky.C <cloki0610@gmail.com>
2022-02-22 14:01:32 +0000

    add model data into database, edit model and admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
+""" Products model display on admin site """
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -8,14 +8,14 @@ class Category(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     class Meta:
-        """ Change display name in admin site """
+        """ Edit display name in admin site """
         verbose_name_plural = 'Categories'
 
     def __str__(self):
         return str(self.name)
 
     def get_friendly_name(self):
-        """ return the friendly name like string method """
+        """ return the friendly name as string method """
         return str(self.friendly_name)
 
 
@@ -26,7 +26,6 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
