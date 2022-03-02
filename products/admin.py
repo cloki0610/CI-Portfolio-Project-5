a889a25dd34ed04d63053f9f56a6fbdefedeed06
Rocky.C <cloki0610@gmail.com>
2022-03-02 22:49:39 +0000

    update toast, adjust comments

--- products/admin.py
+++ products/admin.py
@@ -5,13 +5,13 @@ from .models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    """ Show model in admin site with follow rules """
+    """ Show Category model in admin site with follow rules """
     list_display = ('name', 'friendly_name')
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    """ Show model in admin site with follow rules """
+    """ Show Product model in admin site with follow rules """
     list_filter = ('category',)
     list_display = ('name', 'category', 'description', 'price')
     search_fields = ['name', 'description']
