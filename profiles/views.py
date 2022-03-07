e367bb29c6cc2320e27d99671f41aa50b8c2d9bf
Rocky.C <cloki0610@gmail.com>
2022-03-07 17:28:40 +0000

    edit allauth layout

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
 """ View of profiles """
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.contrib import messages
+from .models import UserProfile
 
 
 class ProfileView(View):
@@ -9,6 +10,9 @@ class ProfileView(View):
 
     def get(self, request):
         """ GET method """
+        profile = get_object_or_404(UserProfile, user=request.user)
         return render(request,
                       "profiles/profile.html",
-                      {})
+                      {
+                          'profile': profile,
+                      })
