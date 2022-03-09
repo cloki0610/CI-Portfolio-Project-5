8822304b6f86ddde0c8e5e7fa3557b2cdc825b9a
Rocky.C <cloki0610@gmail.com>
2022-03-09 17:52:18 +0000

    Add remove user button to normal member

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,4 +7,8 @@ from . import views
 urlpatterns = [
     path('', views.ProfileView.as_view(), name="profile"),
     path('order_history/<order_number>', include('order_history.urls')),
+    path('delete_account/', views.DeleteAccount.as_view(),
+         name='delete_account'),
+    path('delete_action/', views.DeleteAction.as_view(),
+         name='delete_action'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, reverse
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import UserProfileForm
 
 
@@ -14,6 +15,8 @@ class ProfileView(LoginRequiredMixin, View):
         profile = request.user.userprofile
         orders = profile.orders.all()
         profile_form = UserProfileForm(instance=request.user.userprofile)
+        messages.info(request, 'When editing your profile, ' +
+                      'please know that your Full name and email are required.')
         return render(request,
                       "profiles/profile.html",
                       {
@@ -36,3 +39,43 @@ class ProfileView(LoginRequiredMixin, View):
                            'Updated Invalid, Edit and Try Again!')
         # redirect to profile page
         return redirect(reverse('profile'))
+
+
+class DeleteAccount(LoginRequiredMixin, View):
+    """ confirm page for delete account """
+
+    def get(self, request):
+        # return template for confirmation before delete
+        """ get method """
+        if request.user.is_superuser:
+            messages.error(request, 'Request denied, superuser ' +
+                           'should contact our team before remove account.')
+            return redirect(reverse('home'))
+        messages.warning(request,
+                         'You\'re now tring to remove your account. ' +
+                         'Please read our warning message before you leaving.')
+        return render(
+            request,
+            "profiles/delete_account.html",
+        )
+
+
+class DeleteAction(LoginRequiredMixin, View):
+    """ View to delete account after confirmation """
+
+    def post(self, request):
+        """ POST method """
+        if request.user.is_superuser:
+            messages.error(request, 'Request denied, superuser ' +
+                           'should contact our team before remove account.')
+            return redirect(reverse('home'))
+        try:
+            request.user.delete()
+            messages.success(request,
+                             'Thanks for join us and hope to see you again!')
+        # if user record not exist then raise the exception
+        except ObjectDoesNotExist:
+            messages.error(request, "User does not exist.")
+            return redirect(reverse('home'))
+        # whatever the result redirect to home page
+        return redirect(reverse('home'))
