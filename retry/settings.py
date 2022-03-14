37b701d5b49525bf940989a240bcff11235464ff
Rocky.C <cloki0610@gmail.com>
2022-03-14 20:59:39 +0000

    Try to change email sender name

--- retry/settings.py
+++ retry/settings.py
@@ -215,7 +215,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = 'Retry Support'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
